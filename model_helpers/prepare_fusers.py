8ac7794071027db01373805cc88d620cac44dfa5
kijai <40791699+kijai@users.noreply.github.com>
2024-05-06 23:06:14 +0300

    Expose fuser scale (GatedSelfAttentionDense scale)

--- model_helpers/prepare_fusers.py
+++ model_helpers/prepare_fusers.py
@@ -4,12 +4,13 @@ import torch
 from comfy.gligen import GatedSelfAttentionDense as GSAD
 
 class GatedSelfAttentionDense(GSAD):
+    
     def forward(self, x, instance_options={}):
         objs = instance_options['objs']
         x = super().forward(x, objs)
         return x.to(torch.float16)
 
-def prepare_fusers(fusers_ckpt) -> list[torch.nn.Module]:
+def prepare_fusers(fusers_ckpt, fusers_scale) -> list[torch.nn.Module]:
     fusers_list = []
     for key in fusers_ckpt['input_blocks']:
         fusers_ckpt['input_blocks'][key]['params']['query_dim'] = fusers_ckpt['input_blocks'][key]['params']['n_heads'] * \
@@ -17,6 +18,7 @@ def prepare_fusers(fusers_ckpt) -> list[torch.nn.Module]:
         fuser = GatedSelfAttentionDense(
             **fusers_ckpt['input_blocks'][key]['params'])
         fuser.load_state_dict(fusers_ckpt['input_blocks'][key]['state'])
+        fuser.scale = fusers_scale
         fusers_list.append(fuser)
 
     fusers_ckpt['middle_block']['1']['params']['query_dim'] = fusers_ckpt['middle_block']['1']['params']['n_heads'] * \
@@ -24,6 +26,7 @@ def prepare_fusers(fusers_ckpt) -> list[torch.nn.Module]:
     fuser = GatedSelfAttentionDense(
         **fusers_ckpt['middle_block']['1']['params'])
     fuser.load_state_dict(fusers_ckpt['middle_block']['1']['state'])
+    fuser.scale = fusers_scale
     fusers_list.append(fuser)
 
     for key in fusers_ckpt['output_blocks']:
@@ -32,6 +35,7 @@ def prepare_fusers(fusers_ckpt) -> list[torch.nn.Module]:
         fuser = GatedSelfAttentionDense(
             **fusers_ckpt['output_blocks'][key]['params'])
         fuser.load_state_dict(fusers_ckpt['output_blocks'][key]['state'])
+        fuser.scale = fusers_scale
         fusers_list.append(fuser)
 
     return fusers_list
